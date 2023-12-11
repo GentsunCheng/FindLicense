3674fcc16aa90dcee2588ee275e2b673143d53cb
GentsunCheng <oriimiku@gmail.com>
2023-12-11 20:35:11 +0800

    车牌膨胀处理

--- src/sample/recognize.py
+++ src/sample/recognize.py
@@ -11,7 +11,21 @@ class FindLicense(object):
         self.proc_im = self.pre_im
         self.proc_im = cv2.cvtColor(self.proc_im, cv2.COLOR_BGR2GRAY)
         ret, self.proc_im = cv2.threshold(self.proc_im, 127, 255, cv2.THRESH_BINARY)
+        self.im_plate = self.pre_im
+        self.template = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U',
+                         'V', 'W', 'X', 'Y', 'Z',
+                         '藏', '川', '鄂', '甘', '赣', '贵', '桂', '黑', '沪', '吉', '冀', '津', '晋', '京', '辽', '鲁',
+                         '蒙', '闽', '宁',
+                         '青', '琼', '陕', '苏', '皖', '湘', '新', '渝', '豫', '粤', '云', '浙']
+        self.cn_word = self.template[34:69]
+        self.en_word = self.template[10:34]
+        self.tail_word = self.template[0:34]
         pass
+
+    def identify(self):
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+        self.im_plate = cv2.dilate(self.im_plate, kernel)
 
     def get_plate(self, pre_im=None, proc_im=None, debug=False):
         time.sleep(0.05)
@@ -29,6 +43,11 @@ class FindLicense(object):
             aspect_ratio = w / h
             if 2 < aspect_ratio < 5:
                 possible_plates.append((x, y, w, h))
+
+        # 在proc_im上切割出车牌区域
+        for (x, y, w, h) in possible_plates:
+            cv2.rectangle(proc_im, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            self.im_plate = proc_im[y:y + h, x:x + w]
         # 在原图上绘制车牌区域
         for (x, y, w, h) in possible_plates:
             if debug:
